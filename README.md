a4f7be4334f2a4fd59d3db4493cc7ad8b861f336
Perry Roper <perryroper@gmail.com>
2014-02-05 12:59:23 +0000

    Initial project files

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-angular-reverse-url
-===================
+# angular-reverse-url
+
+# Usage
+Reverse URLs from your `$routeProvider` by controller name and URL fragments.
+
+`<a href="{{ 'MyController'|reverseUrl }}">My link</a>`
+
+`<a href="{{ 'MyController'|reverseUrl:{fragment: data} }}">My link</a>`
+
+# Installation
+
+`bower install angular-reverse-url`
---
+++ reverse_url.js
@@ -0,0 +1,22 @@
+(function () {
+    'use strict';
+
+    angular.module('angular-reverse-url', ['ngRoute'])
+        .filter('reverseUrl', ['$route', function ($route) {
+            var regexp = /:([A-Za-z0-9]*)\\*?\\??/g;
+
+            return function (controller, params) {
+                var targetRoute;
+                angular.forEach($route.routes, function (route) {
+                    if (route.controller === controller) {
+                        targetRoute = route.originalPath;
+                        return false;
+                    }
+                });
+                targetRoute = targetRoute.replace(regexp, function (match, pattern) {
+                    return params[pattern];
+                });
+                return '#' + targetRoute;
+            };
+        }]);
+}());

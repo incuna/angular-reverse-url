00f048fe44190a4df276a6c9ddf2a80359ecd1a8
Roger Hutchings <roger@incuna.com>
2014-08-22 10:59:16 +0100

    Add tests for existing functionality

--- test/reverse_urlSpec.js
+++ test/reverse_urlSpec.js
@@ -2,12 +2,49 @@
 
     'use strict';
 
+    var reverseUrl, $route;
+    var routeMock = {};
+
+    routeMock.routes = {
+        '/testRoute1/': {
+            controller: 'TestController1',
+            originalPath: '/test-route-1/'
+        },
+        '/testRoute1/:params/': {
+            controller: 'TestController1',
+            originalPath: '/test-route-1/:param/'
+        },
+        '/testRoute2/': {
+            name: 'TestRoute2',
+            originalPath: '/test-route-2/'
+        },
+    };
+
     describe('Unit: angular-reverse-url', function () {
 
-        beforeEach(module('angular-reverse-url'));
+        beforeEach(module('angular-reverse-url', function ($provide) {
+            $provide.value('$route', routeMock);
+        }));
 
         describe('reverseUrl filter', function () {
-            
+
+            beforeEach(inject(function ($injector) {
+                $route = $injector.get('$route')
+                reverseUrl = $injector.get('$filter')('reverseUrl');
+            }));
+
+            it('should correctly match to a basic route by controller', function () {                
+                expect(reverseUrl('TestController1')).toEqual('#/test-route-1/');
+            });
+
+            it('should correctly match to a basic route by name', function () {                
+                expect(reverseUrl('TestRoute2')).toEqual('#/test-route-2/');
+            });
+
+            it('should correctly match to a route with params', function () {                
+                expect(reverseUrl('TestController1', {param: 'foobar'})).toEqual('#/test-route-1/foobar/');
+            });
+
         });
 
     });
